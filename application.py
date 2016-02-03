5654fe21e5366f4c00a38248659eecfcbcc6c6f6
Jianyong <callmelaoxu@gmail.com>
2016-02-03 16:07:57 +0800

    add google verify file

--- application.py
+++ application.py
@@ -56,6 +56,7 @@ class Application(tornado.web.Application):
             (r"/(bdsitemap\.txt)", tornado.web.StaticFileHandler, dict(path = settings["static_path"])),
             (r"/(orca\.txt)", tornado.web.StaticFileHandler, dict(path = settings["static_path"])),
             (r"/(baidu_verify_cNFzOIiNxf\.html)", tornado.web.StaticFileHandler, dict(path = settings["static_path"])),
+            (r"/(google4a4f69878d81fc8a\.html)", tornado.web.StaticFileHandler, dict(path = settings["static_path"])),
 
             (r"/", handler.index.BbsHandler),
             (r"/p/(\d+)", handler.index.PostHandler),
