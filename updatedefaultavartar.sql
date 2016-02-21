ec32bcf67b198099d7f4cacfec47506544e8172f
zhudewei <zdwxxx@qq.com>
2016-02-21 16:04:59 +0800

    add update default avartar sql

---
+++ updatedefaultavartar.sql
@@ -0,0 +1,7 @@
+UPDATE `1024nj`.`user` SET `avatar`='http://7xr3m9.com1.z0.glb.clouddn.com/logo1.png' WHERE `uid`='1';
+UPDATE `1024nj`.`user` SET `avatar`='http://7xr3m9.com1.z0.glb.clouddn.com/logo1.png' WHERE `uid`='2';
+UPDATE `1024nj`.`user` SET `avatar`='http://7xr3m9.com1.z0.glb.clouddn.com/logo1.png' WHERE `uid`='3';
+UPDATE `1024nj`.`user` SET `avatar`='http://7xr3m9.com1.z0.glb.clouddn.com/logo1.png' WHERE `uid`='4';
+UPDATE `1024nj`.`user` SET `avatar`='http://7xr3m9.com1.z0.glb.clouddn.com/logo1.png' WHERE `uid`='5';
+UPDATE `1024nj`.`user` SET `avatar`='http://7xr3m9.com1.z0.glb.clouddn.com/logo1.png' WHERE `uid`='6';
+
